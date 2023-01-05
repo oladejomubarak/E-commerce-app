9d09a9419c9eebef8da31aee9441917f199bac55
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-05 20:16:23 +0000

    app created

---
+++ src/test/java/com/example/ecommercesystem/ECommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
