167f4330da925a6c0f0b7c07351bae0de486f124
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-07 06:56:20 +0000

    vendor services created

--- src/main/java/com/example/ecommercesystem/services/VendorServices.java
+++ src/main/java/com/example/ecommercesystem/services/VendorServices.java
@@ -1,4 +1,11 @@
 package com.example.ecommercesystem.services;
 
 public interface VendorServices {
+    CreateVendorResponse createVendor(CreateVendorRequest createVendorRequest);
+    LoginResponse login(LoginRequest loginRequest);
+    Response updateVendor(UpdateRequest updateRequest);
+    Response deleteVendor(int id);
+
+    Response addProduct(int id, AddProductRequest addProductRequest);
+
 }
