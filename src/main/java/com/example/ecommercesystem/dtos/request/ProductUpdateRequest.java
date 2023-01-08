58d38cad784577d8447a9444dbc5ce3e7994decd
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-08 03:35:59 +0000

    product update request added

--- src/main/java/com/example/ecommercesystem/dtos/request/ProductUpdateRequest.java
+++ src/main/java/com/example/ecommercesystem/dtos/request/ProductUpdateRequest.java
@@ -1,4 +1,15 @@
 package com.example.ecommercesystem.dtos.request;
 
+import com.example.ecommercesystem.data.model.ProductCategories;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ProductUpdateRequest {
+    public String id;
+    public String name;
+    public BigDecimal price;
+    public ProductCategories productCategories;
+    public int quantity;
 }
