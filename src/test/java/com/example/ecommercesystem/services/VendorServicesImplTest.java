3b6ee15beb0c328edd1d0467257c6fda380d51c3
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-08 23:36:31 +0000

    vendor services test updated

--- src/test/java/com/example/ecommercesystem/services/VendorServicesImplTest.java
+++ src/test/java/com/example/ecommercesystem/services/VendorServicesImplTest.java
@@ -1,9 +1,11 @@
 package com.example.ecommercesystem.services;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class VendorServicesImplTest {
 
     @Autowired
