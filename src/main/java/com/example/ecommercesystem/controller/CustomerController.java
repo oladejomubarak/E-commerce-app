471fa1eaecc8159c78386ff373e7e671b741e5c0
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-09 22:03:46 +0000

    customer controller updated

--- src/main/java/com/example/ecommercesystem/controller/CustomerController.java
+++ src/main/java/com/example/ecommercesystem/controller/CustomerController.java
@@ -1,13 +1,13 @@
 package com.example.ecommercesystem.controller;
 
 import com.example.ecommercesystem.dtos.request.CreateCustomerRequest;
+import com.example.ecommercesystem.dtos.request.LoginRequest;
+import com.example.ecommercesystem.dtos.request.UpdateCustomerRequest;
 import com.example.ecommercesystem.services.CustomerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
@@ -18,6 +18,12 @@ public class CustomerController {
     public ResponseEntity<?> register(@RequestBody CreateCustomerRequest createCustomerRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(customerServices.register(createCustomerRequest));
     }
-
-
+    @GetMapping("/userlogin")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
+        return ResponseEntity.ok(customerServices.login(loginRequest));
+    }
+    @PatchMapping("/updatecustomer")
+    public ResponseEntity<?> updateCustomer(@RequestBody UpdateCustomerRequest updateCustomerRequest){
+        return ResponseEntity.ok(customerServices.updateCustomer(updateCustomerRequest));
+    }
 }
