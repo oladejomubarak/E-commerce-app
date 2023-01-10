244bcf76c759af27d438c5d30ed2529d0fd84d8d
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-10 23:44:00 +0000

    vendor controller updated

--- src/main/java/com/example/ecommercesystem/dtos/response/AddProductResponse.java
+++ src/main/java/com/example/ecommercesystem/dtos/response/AddProductResponse.java
@@ -2,7 +2,9 @@ package com.example.ecommercesystem.dtos.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class AddProductResponse {
