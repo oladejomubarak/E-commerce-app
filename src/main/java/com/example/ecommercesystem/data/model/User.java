675c63e346c8a9d32b526790faa941f38fef1973
oladejomubarak <oladejomubarakade@gmail.com>
2023-07-23 02:19:10 +0100

    commiting.....

--- src/main/java/com/example/ecommercesystem/data/model/User.java
+++ src/main/java/com/example/ecommercesystem/data/model/User.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Data
 public class User {
 //    @Id
 //    private String id;
